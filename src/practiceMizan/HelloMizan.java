f2740a824247cab2bf458a423a1177edb96493e5
LAPTOP-CE9Q5GVC\13475 <mznrmnn@gmail.com>
2022-01-18 00:00:29 -0500

    12.00am

---
+++ src/practiceMizan/HelloMizan.java
@@ -0,0 +1,10 @@
+package practiceMizan;
+
+public class HelloMizan {
+	// main shortcut fr main method
+	// syso shortcut for print
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
